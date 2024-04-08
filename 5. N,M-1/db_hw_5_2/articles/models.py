1d722f298a3ee5ab5cfb366e725c5468d0202424
어지민 <eojm9388@gamil.com>
2024-04-08 11:53:47 +0900

    DB-N:M-1 과제1

---
+++ 5. N,M-1/db_hw_5_2/articles/models.py
@@ -0,0 +1,13 @@
+# articles/models.py
+from django.db import models
+from django.conf import settings
+
+class Article(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    like_users = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='like_users')
+    title = models.CharField(max_length=10)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now=True, auto_now_add=False)
+    updated_at = models.DateTimeField(auto_now=False, auto_now_add=True)
+    
+    
